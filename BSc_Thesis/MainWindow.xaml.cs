84ebeff1d4c6effc6d0dc3182c557fd57ca9ac24
Marek <marek.jakobiak@gmail.com>
2018-11-27 20:44:07 +0100

    Initial commit

---
+++ BSc_Thesis/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using BSc_Thesis.Models;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BSc_Thesis
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Port> ports;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            getPorts();
+            var porto = SerialPort.GetPortNames();
+            Combo.ItemsSource = ports;
+            Combo.SelectedValuePath = "Name";
+        }
+
+        private void getPorts()
+        {
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM WIN32_SerialPort"))
+            {
+                string[] portnames = SerialPort.GetPortNames();
+                var x = searcher.Get().Cast<ManagementBaseObject>().ToList();
+                ports = (from n in portnames
+                         join p in x on n equals p["DeviceID"].ToString() into np
+                         from p in np.DefaultIfEmpty()
+                         select new Port() { Name = n, Desc = p != null ? p["Description"].ToString() : "Brak Opisu" }).ToList();
+            }
+        }
+
+        private void ChangeComListening(object sender, RoutedEventArgs e) {
+            try {
+                SerialPort SP1 = new SerialPort();
+                SP1.PortName = "COM1";
+                SP1.BaudRate = 250;
+                SP1.Parity = Parity.None;
+                SP1.DataBits = 8;
+                SP1.StopBits = StopBits.One;
+                SP1.Handshake = Handshake.RequestToSend;
+                SP1.DtrEnable = true;
+
+            } catch {
+
+            }
+        }
+    }
+}
---
+++ BSc_Thesis/Models/Port.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSc_Thesis.Models
+{
+    class Port
+    {
+        public string Name { get; set; }
+        public string Desc { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} ({1})", Name, Desc);
+        }
+    }
+}
